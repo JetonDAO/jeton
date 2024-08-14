1ef5510560721ae3b77b95bc95f8a9cf0c786d83
Arsalan <ars.salari@gmail.com>
2024-08-14 14:19:46 +0330

    Minor styling improvement

--- apps/web/src/app/@modal/(.)login/page.tsx
+++ apps/web/src/app/@modal/(.)login/page.tsx
@@ -4,9 +4,7 @@ import Modal from "@jeton/ui/Modal";
 export default function LoginModal() {
   return (
     <Modal>
-      <div className="text-center mt-[50vh]">
-        this is login page but opened as modal
-      </div>
+      <div className="text-center">this is login page but opened as modal</div>
     </Modal>
   );
 }
--- packages/ui/src/Modal.tsx
+++ packages/ui/src/Modal.tsx
@@ -35,12 +35,12 @@ export default function Modal({ children }: { children: React.ReactNode }) {
   return (
     <div
       ref={overlay}
-      className="fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/60 p-10"
+      className="fixed z-10 left-0 right-0 top-0 bottom-0 mx-auto bg-black/60"
       onClick={onClick}
     >
       <div
         ref={wrapper}
-        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 sm:w-10/12 md:w-8/12 lg:w-2/5 p-6"
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-6"
       >
         {children}
       </div>
