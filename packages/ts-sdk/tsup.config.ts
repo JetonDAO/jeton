3ab81871bfd1b56bb79636afe86ae83555e0456f
Arsalan <ars.salari@gmail.com>
2024-09-11 12:44:30 +0330

    Fix build

--- packages/ts-sdk/tsup.config.ts
+++ packages/ts-sdk/tsup.config.ts
@@ -6,18 +6,13 @@ export default defineConfig({
   entry: ["src/index.ts", "src/transport/index.ts"],
 
   format: ["cjs", "esm"],
-  splitting: false,
   sourcemap: true,
   clean: true,
   dts: true, // generate dts files
-  minify: false,
+  minify: true,
   env: {
     PIESOCKET_API_KEY: process.env.PIESOCKET_API_KEY || "",
   },
-  noExternal: [
-    "@jeton/zk-deck/wasm/shuffle-encrypt-deck.wasm",
-    "@jeton/zk-deck/wasm/decrypt-card-share.wasm",
-  ],
   loader: {
     ".wasm": "binary",
   },
