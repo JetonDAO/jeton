8ff9176bdcea616549f48b733fa36cdb3be0f872
Arsalan <ars.salari@gmail.com>
2024-10-05 18:46:10 +0330

    Fix tests

--- packages/ts-sdk/jest.config.js
+++ packages/ts-sdk/jest.config.js
@@ -1,9 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
+import { pathsToModuleNameMapper } from "ts-jest";
+import tsConfig from "./tsconfig.json" with { type: "json" };
 export default {
   testEnvironment: "node",
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
   },
+  moduleNameMapper: pathsToModuleNameMapper(tsConfig.compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
   collectCoverage: true,
   //collectCoverageFrom: ["./src/**"]
   collectCoverageFrom: ["./src/BettingManager.ts"],
